4e341181e1853aca13c0cf64e70fa0a1cbd045d3
Sean Kovacs <kovacsmillio@gmail.com>
2026-09-04 21:31:15 -0400

    fix lib/colors.nix

--- lib/colors.nix
+++ lib/colors.nix
@@ -29,71 +29,63 @@ let
   ];
 in
 {
-  options = {
-    colors = {
-      accent = lib.mkOption {
-        description = "Accent color used across the config.";
-        type = lib.types.str;
-      };
-
-      scheme = lib.mkOption {
-        description = "Name of the active color scheme.";
-        type = lib.types.str;
-      };
+  options.colors = {
+    accent = lib.mkOption {
+      default = "base0E";
+      description = "Accent color slot key used across the configuration.";
+      type = lib.types.str;
     };
 
-    scheme =
+    palette =
       lib.genAttrs base24Slots (
-        _:
+        slot:
         lib.mkOption {
+          default =
+            {
+              base00 = "1e1e2e";
+              base01 = "313244";
+              base02 = "45475a";
+              base03 = "6c7086";
+              base04 = "a6adc8";
+              base05 = "cdd6f4";
+              base06 = "f5e0dc";
+              base07 = "b4befe";
+              base08 = "f38ba8";
+              base09 = "fab387";
+              base0A = "f9e2af";
+              base0B = "a6e3a1";
+              base0C = "94e2d5";
+              base0D = "89b4fa";
+              base0E = "cba6f7";
+              base0F = "f2cdcd";
+              base10 = "181825";
+              base11 = "11111b";
+              base12 = "eba0ac";
+              base13 = "f5e0dc";
+              base14 = "a6e3a1";
+              base15 = "89dceb";
+              base16 = "74c7ec";
+              base17 = "f5c2e7";
+            }
+            .${slot};
+
           description = "Hex color, six digits, no leading '#'.";
           type = lib.types.strMatching "[0-9A-Fa-f]{6}";
         }
       )
       // {
         withHashtag = lib.mkOption {
-          description = "Mirror of `config.scheme.*`, each value prefixed with '#'.";
+          default = lib.genAttrs base24Slots (slot: "#" + config.colors.palette.${slot});
+          description = "Mirror of `config.colors.palette.*`, each value prefixed with '#'.";
           readOnly = true;
           type = lib.types.attrsOf lib.types.str;
         };
       };
-  };
 
-  config = {
-    colors = {
-      accent = lib.mkDefault "base0E";
-      scheme = lib.mkDefault "catppuccin-mocha";
+    schemeName = lib.mkOption {
+      default = "catppuccin-mocha";
+      description = "Name of the active color scheme.";
+      type = lib.types.str;
     };
-
-    scheme =
-      (lib.mapAttrs (_: lib.mkDefault) {
-        base00 = "1e1e2e";
-        base01 = "313244";
-        base02 = "45475a";
-        base03 = "6c7086";
-        base04 = "a6adc8";
-        base05 = "cdd6f4";
-        base06 = "f5e0dc";
-        base07 = "b4befe";
-        base08 = "f38ba8";
-        base09 = "fab387";
-        base0A = "f9e2af";
-        base0B = "a6e3a1";
-        base0C = "94e2d5";
-        base0D = "89b4fa";
-        base0E = "cba6f7";
-        base0F = "f2cdcd";
-        base10 = "181825";
-        base11 = "11111b";
-        base12 = "eba0ac";
-        base13 = "f5e0dc";
-        base14 = "a6e3a1";
-        base15 = "89dceb";
-        base16 = "74c7ec";
-        base17 = "f5c2e7";
-      })
-      // {
-        withHashtag = lib.mkDefault (lib.genAttrs base24Slots (name: "#" + config.scheme.${name}));
-      };
   };
 }
